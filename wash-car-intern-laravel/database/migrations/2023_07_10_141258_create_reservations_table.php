fcee810681778be05e3be93aa45891653585a45a
noorhonjol <s12011175@stu.najah.edu>
2023-07-10 17:51:24 +0300

    IWP-7 create project skeleton and git repos

---
+++ wash-car-intern-laravel/database/migrations/2023_07_10_141258_create_reservations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            
+            $table->id();
+            
+            $table->foreignId("worker_id")->constrained("workers")->nullable();
+            
+            $table->foreignId("customer_id")->constrained("customers");
+            
+            $table->foreignId("service_id")->constrained("services");
+
+            $table->string("status")->default("pending");
+
+            $table->decimal("longitude");
+            
+            $table->decimal("latitude");
+ 
+            $table->timestamps();
+ 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
---
+++ wash-car-intern-react/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <h1 className=" text-center text-5xl">
+      hello world
+    </h1>
+  )
+}
+
+export default App
